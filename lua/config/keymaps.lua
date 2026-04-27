6d29086f6e3414a4d5e5fd87e42a3330d95734b3
Oleh Oliinyk <oleh.oliynik.98@gmail.com>
2026-04-27 10:35:06 +0000

    Add show code actions keybinding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { silent = true })
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { silent = true })
 
+-- Experimental
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
