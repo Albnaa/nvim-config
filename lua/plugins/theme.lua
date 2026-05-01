7485cf88c5c6c30b51dd2d426ec69aefe212e258
Oleh Oliinyk <oleh.oliynik.98@gmail.com>
2026-05-01 20:33:36 +0000

    Update nvim theme using 'catpuccing' theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,10 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim",
-  priority=1000,
-  config=function()
-    vim.opt.background = "dark"
-    vim.cmd.colorscheme("oxocarbon")
-  end,	  
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = { flavour = "mocha" },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd([[colorscheme catppuccin]])
+  end,
 }
-
