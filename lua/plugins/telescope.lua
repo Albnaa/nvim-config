d4b622381d4c6887829679549ca75552174b743a
Oleh Oliinyk <oleh.oliynik.98@gmail.com>
2026-05-02 13:26:31 +0000

    Add new keybindings for Telescope plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,11 @@ return {
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     config = function()
-      vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>fd", builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set("n", "<leader>ff", builtin.current_buffer_fuzzy_find, { desc = 'Telescope buffers' })
     end
   }
 }
